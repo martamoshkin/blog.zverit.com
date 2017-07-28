a6f98b5b074ee66c1f6a84514029e4837e256a3e
Artamoshkin Maxim <zverit@gmail.com>
2017-07-29 01:32:59 +0300

    sw

---
+++ js/sw-registration.js
@@ -0,0 +1,38 @@
+function handleRegistration(registration){
+  registration.onupdatefound = (e) => {
+    const installingWorker = registration.installing;
+    installingWorker.onstatechange = (e) => {
+      if (installingWorker.state !== 'installed') return;
+      if (navigator.serviceWorker.controller) {
+        console.log('SW is updated');
+      } else {
+        console.log('A Visit without previous SW');
+        createSnackbar({
+          message: 'App ready for offline use.',
+          duration: 3000
+        })
+      }
+    };
+  }
+}
+
+if(navigator.serviceWorker){
+  navigator.serviceWorker
+    .register('/sw.js')
+    .then((registration) => handleRegistration(registration))
+    .catch((error) => {console.log('ServiceWorker registration failed: ', error)})
+
+  navigator.serviceWorker.onmessage = (e) => {
+    console.log('SW: SW Broadcasting:', event);
+    const data = e.data
+    
+    if(data.command == "UPDATE_FOUND"){
+      console.log("UPDATE_FOUND_BY_SW", data);
+      createSnackbar({
+        message: "Content updated.",
+        actionText:"refresh",
+        action: function(e){location.reload()}
+      })
+    }
+  }
+}
---
+++ sw.js
@@ -0,0 +1,150 @@
+const CACHE_NAMESPACE = 'main-'
+
+const CACHE = CACHE_NAMESPACE + 'precache-then-runtime';
+const PRECACHE_LIST = [
+  "./"
+]
+const HOSTNAME_WHITELIST = [
+  self.location.hostname,
+  "zverit.com",
+  "cdnjs.cloudflare.com",
+  "analytics.truvisibility.com"
+]
+const DEPRECATED_CACHES = ['precache-v1', 'runtime', 'main-precache-v1', 'main-runtime']
+
+
+// The Util Function to hack URLs of intercepted requests
+const getCacheBustingUrl = (req) => {
+  var now = Date.now();
+  url = new URL(req.url)
+
+  url.protocol = self.location.protocol
+
+  url.search += (url.search ? '&' : '?') + 'cache-bust=' + now;
+  return url.href
+}
+
+const isNavigationReq = (req) => (req.mode === 'navigate' || (req.method === 'GET' && req.headers.get('accept').includes('text/html')))
+
+const endWithExtension = (req) => Boolean(new URL(req.url).pathname.match(/\.\w+$/))
+
+const shouldRedirect = (req) => (isNavigationReq(req) && new URL(req.url).pathname.substr(-1) !== "/" && !endWithExtension(req))
+
+const getRedirectUrl = (req) => {
+  url = new URL(req.url)
+  url.pathname += "/"
+  return url.href
+}
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(cache => {
+      return cache.addAll(PRECACHE_LIST)
+        .then(self.skipWaiting())
+        .catch(err => console.log(err))
+    })
+  )
+});
+
+self.addEventListener('activate', event => {
+  // delete old deprecated caches.
+  caches.keys().then(cacheNames => Promise.all(
+    cacheNames
+      .filter(cacheName => DEPRECATED_CACHES.includes(cacheName))
+      .map(cacheName => caches.delete(cacheName))
+  ))
+  console.log('service worker activated.')
+  event.waitUntil(self.clients.claim());
+});
+
+
+var fetchHelper = {
+
+  fetchThenCache: function(request){
+    const init = { mode: "cors", credentials: "omit" } 
+
+    const fetched = fetch(request, init)
+    const fetchedCopy = fetched.then(resp => resp.clone());
+
+    Promise.all([fetchedCopy, caches.open(CACHE)])
+      .then(([response, cache]) => response.ok && cache.put(request, response))
+      .catch(_ => {/* eat any errors */})
+    
+    return fetched;
+  },
+
+  cacheFirst: function(url){
+    return caches.match(url) 
+      .then(resp => resp || this.fetchThenCache(url))
+      .catch(_ => {/* eat any errors */})
+  }
+}
+
+self.addEventListener('fetch', event => {
+  if (HOSTNAME_WHITELIST.indexOf(new URL(event.request.url).hostname) > -1) {
+
+    if (shouldRedirect(event.request)) {
+      event.respondWith(Response.redirect(getRedirectUrl(event.request)))
+      return;
+    }
+
+    // Cache-only Startgies for ys.static resources
+    if (event.request.url.indexOf('ys.static') > -1){
+      event.respondWith(fetchHelper.cacheFirst(event.request.url))
+      return;
+    }
+
+    const cached = caches.match(event.request);
+    const fetched = fetch(getCacheBustingUrl(event.request), { cache: "no-store" });
+    const fetchedCopy = fetched.then(resp => resp.clone());
+
+    event.respondWith(
+      Promise.race([fetched.catch(_ => cached), cached])
+        .then(resp => resp || fetched)
+        .catch(_ => caches.match('offline.html'))
+    );
+
+    event.waitUntil(
+      Promise.all([fetchedCopy, caches.open(CACHE)])
+        .then(([response, cache]) => response.ok && cache.put(event.request, response))
+        .catch(_ => {/* eat any errors */ })
+    );
+
+    if (isNavigationReq(event.request)) {
+      console.log(`fetch ${event.request.url}`)
+      event.waitUntil(revalidateContent(cached, fetchedCopy))
+    }
+  }
+});
+
+
+function sendMessageToAllClients(msg) {
+  self.clients.matchAll().then(clients => {
+    clients.forEach(client => {
+      console.log(client);
+      client.postMessage(msg)
+    })
+  })
+}
+
+function sendMessageToClientsAsync(msg) {
+  setTimeout(() => {
+    sendMessageToAllClients(msg)
+  }, 1000)
+}
+
+function revalidateContent(cachedResp, fetchedResp) {
+  return Promise.all([cachedResp, fetchedResp])
+    .then(([cached, fetched]) => {
+      const cachedVer = cached.headers.get('last-modified')
+      const fetchedVer = fetched.headers.get('last-modified')
+      console.log(`"${cachedVer}" vs. "${fetchedVer}"`);
+      if (cachedVer !== fetchedVer) {
+        sendMessageToClientsAsync({
+          'command': 'UPDATE_FOUND',
+          'url': fetched.url
+        })
+      }
+    })
+    .catch(err => console.log(err))
+}
