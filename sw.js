c8435bf43dc4fb6ec4c1b69275f9c46c2849c281
Artamoshkin Maxim <zverit@gmail.com>
2017-07-29 01:43:18 +0300

    fix

--- sw.js
+++ sw.js
@@ -2,13 +2,15 @@ const CACHE_NAMESPACE = 'main-'
 
 const CACHE = CACHE_NAMESPACE + 'precache-then-runtime';
 const PRECACHE_LIST = [
-  "./"
+  "./",
+  "//fonts.googleapis.com/css?family=Roboto+Slab%3A400%7CArimo%3A400%2C400italic%2C700%2C700italic%7CArimo%3A400%2C700%7CRoboto+Slab%3A400&subset=latin%2Ccyrillic&ver=4.7.4"
 ]
 const HOSTNAME_WHITELIST = [
   self.location.hostname,
   "zverit.com",
   "cdnjs.cloudflare.com",
-  "analytics.truvisibility.com"
+  "analytics.truvisibility.com",
+  "google.com"
 ]
 const DEPRECATED_CACHES = ['precache-v1', 'runtime', 'main-precache-v1', 'main-runtime']
 
