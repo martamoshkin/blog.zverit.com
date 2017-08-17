010301fd092f34f1229e2a1ac71e7a3d7cd64ec9
Artamoshkin Maxim <zverit@gmail.com>
2017-08-17 22:43:09 +0300

    remove log

--- js/sw-registration.js
+++ js/sw-registration.js
@@ -14,16 +14,9 @@ if(navigator.serviceWorker){
     .catch((error) => {console.log('ServiceWorker registration failed: ', error)})
 
   navigator.serviceWorker.onmessage = (e) => {
-    console.log('SW: SW Broadcasting:', event);
     const data = e.data
     
     if(data.command == "UPDATE_FOUND"){
-      console.log("UPDATE_FOUND_BY_SW", data);
-      createSnackbar({
-        message: "Content updated.",
-        actionText:"refresh",
-        action: function(e){location.reload()}
-      })
     }
   }
 }
