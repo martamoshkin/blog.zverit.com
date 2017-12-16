ddbdbf5cec00a81af4f9fa810408361298aa7fcf
Artamoshkin Maxim <zverit@gmail.com>
2017-12-16 15:40:29 +0300

    fix page events

--- js/events.js
+++ js/events.js
@@ -1,33 +1,35 @@
-var siteTitle = document.getElementsByClassName('site-title')[0];
-siteTitle.onclick = logoClick;
+window.onload = function () {
+	var siteTitle = document.getElementsByClassName('site-title')[0];
+	siteTitle.onclick = logoClick;
 
-var pageLinkAboutMe = document.getElementsByClassName('page-link about-me')[0];
-pageLinkAboutMe.onclick = pageLinkAboutMeClick;
+	var pageLinkAboutMe = document.getElementsByClassName('page-link about-me')[0];
+	pageLinkAboutMe.onclick = pageLinkAboutMeClick;
 
 
-var pagination = document.getElementsByClassName('pagination')[0];
-pagination.onclick = paginationClick;
+	var pagination = document.getElementsByClassName('pagination')[0];
+	pagination.onclick = paginationClick;
 
-function logoClick() {
-	ga('send', {
-  'hitType': 'event',
-  'eventCategory': 'Navigation',
-  'eventAction': 'Click',
-  'eventLabel': 'Logo'});
-}
+	function logoClick() {
+		ga('send', {
+	  'hitType': 'event',
+	  'eventCategory': 'Navigation',
+	  'eventAction': 'Click',
+	  'eventLabel': 'Logo'});
+	}
 
-function pageLinkAboutMeClick() {
-	ga('send', {
-  'hitType': 'event',
-  'eventCategory': 'Navigation',
-  'eventAction': 'Click',
-  'eventLabel': 'PageLink'});
-}
+	function pageLinkAboutMeClick() {
+		ga('send', {
+	  'hitType': 'event',
+	  'eventCategory': 'Navigation',
+	  'eventAction': 'Click',
+	  'eventLabel': 'AboutMePageLink'});
+	}
 
-function paginationClick() {
-	ga('send', {
-  'hitType': 'event',
-  'eventCategory': 'Navigation',
-  'eventAction': 'Click',
-  'eventLabel': 'Pagination'});
+	function paginationClick() {
+		ga('send', {
+	  'hitType': 'event',
+	  'eventCategory': 'Navigation',
+	  'eventAction': 'Click',
+	  'eventLabel': 'Pagination'});
+	}
 }
