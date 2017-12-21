c0750528873b1aca57f206f95eaf0d73b24e8bfd
Artamoshkin Maxim <zverit@gmail.com>
2017-12-21 08:25:01 +0300

    Update events.js

--- js/events.js
+++ js/events.js
@@ -7,14 +7,18 @@ window.onload = function () {
 
 
 	var pagination = document.getElementsByClassName('pagination')[0];
-	pagination.onclick = paginationClick;
+	if (pagination) {
+		pagination.onclick = paginationClick;
+	}
 
 	var related = document.getElementsByClassName('related')[0];
-	//related.onclick = relatedClick;
-	
+	if (related) {
+	related.onclick = relatedClick;
+	}
 	var share = document.getElementsByClassName('share-buttons')[0];
-	//share.onclick = shareClick;
-	
+	if (share) {
+	share.onclick = shareClick;
+	}
 	function logoClick() {
 		ga('send', {
 	  'hitType': 'event',
