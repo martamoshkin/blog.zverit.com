b7c3098ab97499eaab51539f392dc3dcaf1bba6c
Artamoshkin Maxim <zverit@gmail.com>
2018-01-01 15:24:35 +0300

    added events

--- js/events.js
+++ js/events.js
@@ -13,12 +13,64 @@ window.onload = function () {
 
 	var related = document.getElementsByClassName('related')[0];
 	if (related) {
-	related.onclick = relatedClick;
+		related.onclick = relatedClick;
 	}
+	
 	var share = document.getElementsByClassName('share-buttons')[0];
 	if (share) {
-	share.onclick = shareClick;
+		share.onclick = shareClick;
 	}
+	
+	var pageLinkArchve = document.getElementsByClassName('page-link archive')[0];
+	pageLinkArchive.onclick = pageLinkArchiveClick;
+	
+	var pageLinkCategory = document.getElementsByClassName('page-link category')[0];
+	pageLinkCategory.onclick = pageLinkCategoryClick;
+	
+	var pageLinkTags = document.getElementsByClassName('page-link tags')[0];
+	pageLinkTags.onclick = pageLinkTagsClick;
+	
+	
+	var rss = document.getElementsByClassName('rss-subscribe')[0];
+	rss.onclick = rssClick;
+	
+	var shareButtons =  document.getElementsByClassName('share-buttons')[0];
+	if(shareButtons) {
+		shareButtons.onclick = shareClick;
+	}
+
+	function rssClick() {
+		ga('send', {
+	  'hitType': 'event',
+	  'eventCategory': 'Subscribe',
+	  'eventAction': 'Click',
+	  'eventLabel': 'RSS'});
+	}	
+	
+	function pageLinkArchveClick() {
+		ga('send', {
+	  'hitType': 'event',
+	  'eventCategory': 'Navigation',
+	  'eventAction': 'Click',
+	  'eventLabel': 'Archive'});
+	}
+	
+	function pageLinkCategoryClick() {
+		ga('send', {
+	  'hitType': 'event',
+	  'eventCategory': 'Navigation',
+	  'eventAction': 'Click',
+	  'eventLabel': 'Category'});
+	}
+	
+	function pageLinkTagsClick() {
+		ga('send', {
+	  'hitType': 'event',
+	  'eventCategory': 'Navigation',
+	  'eventAction': 'Click',
+	  'eventLabel': 'Tags'});
+	}
+	
 	function logoClick() {
 		ga('send', {
 	  'hitType': 'event',
