5aee390e5151f836c907ffbd1096338bbfb9f806
Maxim Artamoshkin <msartamoshkin@vtb.ru>
2023-03-04 19:06:44 +0300

    fix

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAMESPACE = 'main-'
+const CACHE_NAMESPACE = 'main-';
 
 const CACHE = CACHE_NAMESPACE + 'precache-then-runtime';
 const PRECACHE_LIST = [
@@ -11,47 +11,47 @@ const PRECACHE_LIST = [
   "//disqus.com/next/config.js",
   "//c.disquscdn.com/uploads/users/21136/5778/avatar92.jpg?1513880609",
   "//c.disquscdn.com/next/current/embed/lang/ru.js"
-  
-]
+
+];
 const HOSTNAME_WHITELIST = [
   self.location.hostname,
   "//zverit.com",
   "cdnjs.cloudflare.com"
-]
-const DEPRECATED_CACHES = ['precache-v1', 'runtime', 'main-precache-v1', 'main-runtime']
+];
+const DEPRECATED_CACHES = ['precache-v1', 'runtime', 'main-precache-v1', 'main-runtime'];
 
 
 // The Util Function to hack URLs of intercepted requests
 const getCacheBustingUrl = (req) => {
   var now = Date.now();
-  url = new URL(req.url)
+  url = new URL(req.url);
 
-  url.protocol = self.location.protocol
+  url.protocol = self.location.protocol;
 
   url.search += (url.search ? '&' : '?') + 'cache-bust=' + now;
-  return url.href
-}
+  return url.href;
+};
 
-const isNavigationReq = (req) => (req.mode === 'navigate' || (req.method === 'GET' && req.headers.get('content-type').includes('text/html')))
+const isNavigationReq = (req) => (req.mode === 'navigate' || (req.method === 'GET' && req.headers.get('content-type').includes('text/html')));
 
-const endWithExtension = (req) => Boolean(new URL(req.url).pathname.match(/\.\w+$/))
+const endWithExtension = (req) => Boolean(new URL(req.url).pathname.match(/\.\w+$/));
 
-const shouldRedirect = (req) => (isNavigationReq(req) && new URL(req.url).pathname.substr(-1) !== "/" && !endWithExtension(req))
+const shouldRedirect = (req) => (isNavigationReq(req) && new URL(req.url).pathname.substr(-1) !== "/" && !endWithExtension(req));
 
 const getRedirectUrl = (req) => {
-  url = new URL(req.url)
-  url.pathname += "/"
-  return url.href
-}
+  url = new URL(req.url);
+  url.pathname += "/";
+  return url.href;
+};
 
 self.addEventListener('install', e => {
   e.waitUntil(
     caches.open(CACHE).then(cache => {
       return cache.addAll(PRECACHE_LIST)
         .then(self.skipWaiting())
-        .catch(err => console.log(err))
+        .catch(err => console.log(err));
     })
-  )
+  );
 });
 
 self.addEventListener('activate', event => {
@@ -60,43 +60,42 @@ self.addEventListener('activate', event => {
     cacheNames
       .filter(cacheName => DEPRECATED_CACHES.includes(cacheName))
       .map(cacheName => caches.delete(cacheName))
-  ))
+  ));
   event.waitUntil(self.clients.claim());
 });
 
 
 var fetchHelper = {
 
-  fetchThenCache: function(request){
-    const init = { mode: "no-cors", credentials: "omit" } 
+  fetchThenCache: function (request) {
+    const init = { mode: "no-cors", credentials: "omit" };
 
-    const fetched = fetch(request, init)
+    const fetched = fetch(request, init);
     const fetchedCopy = fetched.then(resp => resp.clone());
 
     Promise.all([fetchedCopy, caches.open(CACHE)])
       .then(([response, cache]) => response.ok && cache.put(request, response))
-      .catch(_ => {/* eat any errors */})
-    
+      .catch(_ => {/* eat any errors */ });
+
     return fetched;
   },
 
-  cacheFirst: function(url){
-    return caches.match(url) 
+  cacheFirst: function (url) {
+    return caches.match(url)
       .then(resp => resp || this.fetchThenCache(url))
-      .catch(_ => {/* eat any errors */})
+      .catch(_ => {/* eat any errors */ });
   }
-}
+};
 
 self.addEventListener('fetch', event => {
   if (HOSTNAME_WHITELIST.indexOf(new URL(event.request.url).hostname) > -1) {
-
     if (shouldRedirect(event.request)) {
-      event.respondWith(Response.redirect(getRedirectUrl(event.request)))
+      event.respondWith(Response.redirect(getRedirectUrl(event.request)));
       return;
     }
 
-    if (event.request.url.indexOf('ys.static') > -1){
-      event.respondWith(fetchHelper.cacheFirst(event.request.url))
+    if (event.request.url.indexOf('ys.static') > -1) {
+      event.respondWith(fetchHelper.cacheFirst(event.request.url));
       return;
     }
 
@@ -117,7 +116,7 @@ self.addEventListener('fetch', event => {
     );
 
     if (isNavigationReq(event.request)) {
-      event.waitUntil(revalidateContent(cached, fetchedCopy))
+      event.waitUntil(revalidateContent(cached, fetchedCopy));
     }
   }
 });
@@ -126,28 +125,28 @@ self.addEventListener('fetch', event => {
 function sendMessageToAllClients(msg) {
   self.clients.matchAll().then(clients => {
     clients.forEach(client => {
-      client.postMessage(msg)
-    })
-  })
+      client.postMessage(msg);
+    });
+  });
 }
 
 function sendMessageToClientsAsync(msg) {
   setTimeout(() => {
-    sendMessageToAllClients(msg)
-  }, 1000)
+    sendMessageToAllClients(msg);
+  }, 1000);
 }
 
 function revalidateContent(cachedResp, fetchedResp) {
   return Promise.all([cachedResp, fetchedResp])
     .then(([cached, fetched]) => {
-      const cachedVer = cached.headers.get('last-modified')
-      const fetchedVer = fetched.headers.get('last-modified')
-      if (cachedVer !== fetchedVer) {
-        sendMessageToClientsAsync({
-          'command': 'UPDATE_FOUND',
-          'url': fetched.url
-        })
-      }
+      const cachedVer = cached.headers.get('last-modified');
+      const fetchedVer = fetched.headers.get('last-modified');
+      // if (cachedVer !== fetchedVer) {
+      sendMessageToClientsAsync({
+        'command': 'UPDATE_FOUND',
+        'url': fetched.url
+      });
+      // }
     })
-    .catch(err => console.log(err))
+    .catch(err => console.log(err));
 }
